81661ffbcfcaacb670375c111978f27e65441e2b
mathan31 <mathan.chandrasekar@gmail.com>
2020-07-19 17:34:18 +0530

    Devops Implemetation

---
+++ Devops_Final/src/test/java/day01/InvokeChrome.java
@@ -0,0 +1,18 @@
+package day01;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class InvokeChrome {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		System.out.println("Title is : "+driver.getTitle());
+		driver.quit();
+	}
+
+}
